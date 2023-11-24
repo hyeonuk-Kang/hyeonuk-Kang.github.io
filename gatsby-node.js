b00b712b9460233343ae1a5bbc2dc1d90a765b40
hyeonuk.kang <hyeonuk.kang@vuno.co>
2023-11-24 11:43:52 +0900

    Initial Commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,9 +10,27 @@
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
+    path: '/index',
+    component: require.resolve('./src/pages/index.tsx'),
     context: {},
     defer: true,
+  })
+}
+
+const path = require('path')
+
+// Setup Import Alias
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {}
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, 'src/components'),
+        utils: path.resolve(__dirname, 'src/utils'),
+        hooks: path.resolve(__dirname, 'src/hooks'),
+      },
+    },
   })
 }
--- src/components/seo.js
+++ src/components/seo.js
@@ -5,23 +5,21 @@
  * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
  */
 
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import * as React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
 function Seo({ description, title, children }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
+  const { site } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+          author
         }
       }
-    `
-  )
+    }
+  `)
 
   const metaDescription = description || site.siteMetadata.description
   const defaultTitle = site.siteMetadata?.title
